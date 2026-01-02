5c4e0ea7327f25d091d46226c7f46408371a7758
Aatika Yousuf <aatikayousuf127@gmail.com>
2026-01-02 19:18:36 +0500

    Nav Bar Login Button displayed and redirection to dashboardsn fixed

---
+++ NovellaMart/NovellaMart/Core/BL/Services/AuthStateService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NovellaMart.Core.BL.Services
+{
+    public class AuthStateService
+    {
+        // This event triggers whenever login or logout happens
+        public event Action OnChange;
+
+        public void NotifyStateChanged() => OnChange?.Invoke();
+    }
+}
